094d1f5b954787f4854d6bf5d357fb7d924b84fc
Hex Fish <0xhexfish@gmail.com>
2022-07-13 22:17:26 -0400

    Show me bits impl

--- src/main.rs
+++ src/main.rs
@@ -68,7 +68,88 @@ fn value_from_string(input: String) -> Result<u64, Box<dyn Error>> {
 
 fn show_me_bits(input: &String, chunk_size: u8) {
     let input_val = value_from_string(input.to_string()).unwrap();
-    println!("Input: 0x{:x}", input_val);
+
+    // Deduce the number of bits need to represent the input value
+    let u32_max = (1u64 << 32) - 1;
+    let num_bits: u64 = if input_val > u32_max {
+        64u64
+    } else {
+        32u64
+    };
+
+    // Table with chunk size 1
+    //
+    // 32  31  30
+    //   +---+---+
+    //   | 0 | 0 |
+    //   +---+---+
+    //
+    // Table with chunk size 2
+    //
+    // 32   30   28
+    //   +----+----+
+    //   | 00 | 00 |
+    //   +----+----+
+    //
+    // Table with chunk size 4
+    //
+    // 32     28     24
+    //   +------+------+
+    //   | 0000 | 0000 |
+    //   +------+------+
+
+    // Write the table header index values
+    let mut table = String::new();
+    for header_idx in (0..num_bits + 1).rev().step_by(chunk_size as usize) {
+        if chunk_size == 1u8 {
+            table.push_str(&format!("{: >3}|", header_idx).to_string());
+        } else if chunk_size == 2u8 {
+            table.push_str(&format!("{: >4}|", header_idx).to_string());
+        } else {
+            table.push_str(&format!("{: >6}|", header_idx).to_string());
+        };
+    }
+    table.push_str("\n");
+
+    let table_line = if chunk_size == 1u8 {
+        let raw_line = "+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---";
+        if num_bits == 64 {
+            format!("   {}{}+\n   ", raw_line, raw_line)
+        } else {
+            format!("   {}+\n   ", raw_line)
+        }
+    } else if chunk_size == 2u8 {
+        let raw_line = "+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----";
+        if num_bits == 64 {
+            format!("    {}{}+\n    ", raw_line, raw_line)
+        } else {
+            format!("    {}+\n    ", raw_line)
+        }
+    } else {
+        let raw_line = "+------+------+------+------+------+------+------+------";
+        if num_bits == 64 {
+            format!("      {}{}+\n      ", raw_line, raw_line)
+        } else {
+            format!("      {}+\n      ", raw_line)
+        }
+    };
+    table.push_str(&table_line);
+
+    // Write each value
+    for bit_idx in (0..(num_bits - chunk_size as u64) + 1).rev().step_by(chunk_size as usize) {
+        if chunk_size == 1u8 {
+            table.push_str(&format!("| {:b} ", ((input_val >> bit_idx) & 0b1)).to_string());
+        } else if chunk_size == 2u8 {
+            table.push_str(&format!("| {:0>2b} ", ((input_val >> bit_idx) & 0b11)).to_string());
+        } else {
+            table.push_str(&format!("| {:0>4b} ", ((input_val >> bit_idx) & 0b1111)).to_string());
+        }
+    }
+    table.push_str("|\n");
+
+    table.push_str(&table_line);
+
+    println!("{}", table);
 }
 
 fn show_me_signed(input: &String, output_format: OutputFormat) {}
